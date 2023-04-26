2d546765d01bfe7229a3dffc275da3ea29f4ce2b
Brian Faust <hello@basecode.sh>
2023-04-26 04:06:47 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,44 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        </div>
+
+        @if (session('status') == 'verification-link-sent')
+            <div class="mb-4 text-sm font-medium text-green-600 dark:text-green-400">
+                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
+            </div>
+        @endif
+
+        <div class="mt-4 flex items-center justify-between">
+            <form method="POST" action="{{ route('verification.send') }}">
+                @csrf
+
+                <div>
+                    <x-button type="submit">
+                        {{ __('Resend Verification Email') }}
+                    </x-button>
+                </div>
+            </form>
+
+            <div>
+                <a href="{{ route('profile.show') }}"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
+                    {{ __('Edit Profile') }}</a>
+
+                <form method="POST" action="{{ route('logout') }}" class="inline">
+                    @csrf
+
+                    <button type="submit"
+                        class="ml-2 rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </x-authentication-card>
+</x-guest-layout>
